9c7b15960cfa5a573e4a1d2f0a96155dd07a043a
Summer Liu <ssdf90771@gmail.com>
2023-03-05 17:57:21 +0800

    Move shell script and modify some logic

--- script/sniffer.sh
+++ script/sniffer.sh
@@ -5,21 +5,21 @@ export PATH=/opt/homebrew/bin/:$PATH
 you=you-get
 dirLocation=$(cd `dirname $0`; pwd)
 #配置参数
-telegram_bot_token=$(jq -c -r .telegram_bot_token "$dirLocation"/config/config.json)
-telegram_chat_id=$(jq -c -r .telegram_chat_id "$dirLocation"/config/config.json)
-uid=$(jq -c -r .uid "$dirLocation"/config/config.json)
-fid=$(jq -c -r .fid "$dirLocation"/config/config.json)
-rssDomain=$(jq -c -r .rssDomain "$dirLocation"/config/config.json)
+telegram_bot_token=$(jq -c -r .telegram_bot_token "$dirLocation"/config.json)
+telegram_chat_id=$(jq -c -r .telegram_chat_id "$dirLocation"/config.json)
+uid=$(jq -c -r .uid "$dirLocation"/config.json)
+fid=$(jq -c -r .fid "$dirLocation"/config.json)
+rssDomain=$(jq -c -r .rssDomain "$dirLocation"/config.json)
 videoLocation="/usr/you-get-download/"
-cookies_location="$dirLocation"/config/cookies.txt
-bv_location="$dirLocation"/config/BV.txt
+cookies_location="$dirLocation"/cookies.txt
+bv_location="$dirLocation"/BV.txt
 
 favURL="https://space.bilibili.com/$uid/favlist?fid=$fid"
 rssURL="$rssDomain/bilibili/fav/$uid/$fid/1"
 
 #rss 可访问性检测
 response=$(curl -I -m 10 -o /dev/null -s -w %{http_code} "$rssURL")
-if [ "$response" != 200 ]; then
+if [ "$response" != 200 ] && [ "$telegram_bot_token" != "" ]; then
   curl -s -X POST "https://api.telegram.org/bot$telegram_bot_token/sendMessage" -d chat_id="$telegram_chat_id" -d parse_mode=html -d text="$rssURL: RSS 访问失效"
   exit
 fi
@@ -38,7 +38,7 @@ stat=$($you -i -l -c "$cookies_location" https://www.bilibili.com/video/BV1fK4y1
 subStat=${stat#*quality:}
 data=${subStat%%#*}
 quality=${data%%size*}
-if ! [[ $quality =~ "4K" ]]; then
+if ! [[ $quality =~ "4K" ]] && [ "$telegram_bot_token" != "" ]; then
     curl -s -X POST "https://api.telegram.org/bot$telegram_bot_token/sendMessage" -d chat_id=$telegram_chat_id -d parse_mode=html -d text="$favTitle: Cookies 文件失效，请更新后重试"
     exit
 fi
@@ -103,7 +103,9 @@ for(( i=${#infoArray[@]} - 1;i >= 0;i--)) do
     else
       downloadResult="下载出现异常"
     fi
-    curl -s -X POST "https://api.telegram.org/bot$telegram_bot_token/sendMessage" -d chat_id="$telegram_chat_id" -d parse_mode=html -d text="<a href=\"${link}\">$videoTitle</a>%0A${downloadResult}"
+    if [ "$telegram_bot_token" != "" ]; then
+      curl -s -X POST "https://api.telegram.org/bot$telegram_bot_token/sendMessage" -d chat_id="$telegram_chat_id" -d parse_mode=html -d text="<a href=\"${link}\">$videoTitle</a>%0A${downloadResult}"
+    fi
     #记录此次任务中已下载的视频bv
     bvDownloaded[$BVCount]=$bv
     ((BVCount++))
@@ -118,4 +120,6 @@ if [ ${#bvDownloaded[*]} = 0 ]; then
 else
   jobResult='收藏夹本轮检测完成'
 fi
-curl -s -X POST "https://api.telegram.org/bot$telegram_bot_token/sendMessage" -d chat_id="$telegram_chat_id" -d parse_mode=html -d text="<a href=\"${favURL}\">$favTitle</a>%0A${jobResult}"
+if [ "$telegram_bot_token" != "" ]; then
+  curl -s -X POST "https://api.telegram.org/bot$telegram_bot_token/sendMessage" -d chat_id="$telegram_chat_id" -d parse_mode=html -d text="<a href=\"${favURL}\">$favTitle</a>%0A${jobResult}"
+fi
