86c2ededdca95a9ab94204d64a2f2bdac5ef90ec
Summer Liu <ssdf90771@gmail.com>
2023-03-11 16:56:54 +0800

    Fix the issue about useEffect invoke twice

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false, // true 会导致 useEffect(func, []) 执行两次
   output: 'standalone',
 }
 
