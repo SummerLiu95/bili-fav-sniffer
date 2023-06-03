ed26ab874cf8c93d30083be654bad3c65d0861e0
BarryLiu1995 <ssdf90771@gmail.com>
2023-06-03 19:30:13 +0800

    chore(changlog): add standard-version lib

---
+++ .versionrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "types": [
+        { "type": "feat", "section": "✨ Features" },
+        { "type": "fix", "section": "🐛 Bug Fixes" },
+        { "type": "docs", "section": "✏️ Documentation" },
+        { "type": "style", "section": "💄 Styles | 风格" },
+        { "type": "refactor", "section": "♻️ Code Refactoring" },
+        { "type": "perf", "section": "⚡ Performance Improvements" },
+        { "type": "test", "section": "✅ Tests" },
+        { "type": "build", "section": "📦‍ Build System" },
+        { "type": "ci", "section": "👷 Continuous Integration" },
+        { "type": "chore", "section": "🚀 Chore" },
+        { "type": "revert", "section": "⏪ Revert", "hidden": true }
+    ]
+}
